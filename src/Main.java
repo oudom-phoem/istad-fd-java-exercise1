bdb7a6b03c1787ae8faee66c6ba8fc4df9101b1f
Oudom Phoem <oudomphoem@gmail.com>
2024-12-09 15:07:08 +0700

    Let user input a seat to book

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,9 @@ public class Main {
         System.out.print("Please enter number of columns: ");
         byte columns = scanner.nextByte();
 
-        String[][] seats = new String[rows][columns];
+        int[][] seats = new int[rows][columns];
+
+        seats[0][0] = 1;
 
         switch (choice) {
             case 1:
@@ -32,15 +34,27 @@ public class Main {
                     System.out.print("[");
                     for (int j = 0; j < columns; j++) {
                         String seatCode = charRow + "-" + (j + 1) + ":";
-                        String bookStatus = j == columns - 1 ? "AV" : "AV, ";
+                        String bookStatus = "";
+                        if (seats[i][j] == 0) {
+                            bookStatus = j == columns - 1 ? "AV" : "AV, ";
+                        } else {
+                            bookStatus = j == columns - 1 ? "BO" : "BO, ";
+                        }
                         System.out.print(seatCode + bookStatus);
                     }
                     System.out.print("]");
                     charRow++;
                     System.out.println();
                 }
-
                 break;
+            case 2:
+                System.out.print("Please enter the seat code to book (e.g., B3): ");
+                String seatCodeToBook = scanner.nextLine();
+                String seatCodeLetter = seatCodeToBook.substring(0, 1);
+                String seatCodeNumber = seatCodeToBook.substring(1);
+                System.out.println(seatCodeLetter);
+                System.out.println(seatCodeNumber);
+
         }
 
     }
