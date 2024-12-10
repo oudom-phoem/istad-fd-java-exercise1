7005f0968bef096eb03511a79d6d1b02b52f4460
Oudom Phoem <oudomphoem@gmail.com>
2024-12-10 21:32:27 +0700

    Add Booking seat functionality

--- src/Main.java
+++ src/Main.java
@@ -5,56 +5,85 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("""
-                Cinema Hall Seat Booking Application
-                Menu:
-                1. Set up the seat
-                2. Book a seat
-                3. Cancel a booking
-                4. Display seats
-                5. View booking history
-                6. Exit
-                >\s""");
-        byte choice = scanner.nextByte();
+        int[][] seats = null;
 
-        System.out.print("Please enter number of rows: ");
-        byte rows = scanner.nextByte();
+        while (true){
+            System.out.print("""
+                    Cinema Hall Seat Booking Application
+                    Menu:
+                    1. Set up the seat
+                    2. Book a seat
+                    3. Cancel a booking
+                    4. Display seats
+                    5. View booking history
+                    6. Exit
+                    >\s""");
+            byte choice = scanner.nextByte();
 
-        System.out.print("Please enter number of columns: ");
-        byte columns = scanner.nextByte();
+            switch (choice) {
+                case 1:
+                    System.out.print("Please enter number of rows: ");
+                    byte rows = scanner.nextByte();
 
-        int[][] seats = new int[rows][columns];
+                    System.out.print("Please enter number of columns: ");
+                    byte columns = scanner.nextByte();
 
-        seats[0][0] = 1;
+                    seats = new int[rows][columns];
 
-        switch (choice) {
-            case 1:
-                char charRow = 'A';
-                for (int i = 0; i < rows; i++) {
-                    System.out.print("[");
-                    for (int j = 0; j < columns; j++) {
-                        String seatCode = charRow + "-" + (j + 1) + ":";
-                        String bookStatus = "";
-                        if (seats[i][j] == 0) {
-                            bookStatus = j == columns - 1 ? "AV" : "AV, ";
-                        } else {
-                            bookStatus = j == columns - 1 ? "BO" : "BO, ";
+                    char charRow = 'A';
+
+                    for (int i = 0; i < rows; i++) {
+                        System.out.print("[");
+                        for (int j = 0; j < columns; j++) {
+                            String seatCode = charRow + "-" + (j + 1) + ":";
+                            String bookStatus = "";
+                            if (seats[i][j] == 0) {
+                                bookStatus = j == columns - 1 ? "AV" : "AV, ";
+                            } else {
+                                bookStatus = j == columns - 1 ? "BO" : "BO, ";
+                            }
+                            System.out.print(seatCode + bookStatus);
                         }
-                        System.out.print(seatCode + bookStatus);
+                        System.out.print("]");
+                        charRow++;
+                        System.out.println();
                     }
-                    System.out.print("]");
-                    charRow++;
                     System.out.println();
-                }
-                break;
-            case 2:
-                System.out.print("Please enter the seat code to book (e.g., B3): ");
-                String seatCodeToBook = scanner.nextLine();
-                String seatCodeLetter = seatCodeToBook.substring(0, 1);
-                String seatCodeNumber = seatCodeToBook.substring(1);
-                System.out.println(seatCodeLetter);
-                System.out.println(seatCodeNumber);
+                    break;
+                case 2:
+                    if (seats == null) {
+                        System.out.println("Seats have not been set up. Please set up the seats first.");
+                    } else {
+                        scanner.nextLine();
+                        System.out.print("Please enter the seat code to book (e.g., B3): ");
+                        String seatCodeToBook = scanner.nextLine();
+                        char seatRowChar = seatCodeToBook.charAt(0);
+                        String seatColumnStr = seatCodeToBook.substring(1);
 
+                        int seatRowIndex = seatRowChar - 'A';
+                        int seatColumnIndex = Integer.parseInt(seatColumnStr) - 1;
+
+                        if (seatRowIndex >= 0 && seatRowIndex < seats.length && seatColumnIndex >= 0 && seatColumnIndex < seats[0].length) {
+                            if (seats[seatRowIndex][seatColumnIndex] == 0) {
+                                seats[seatRowIndex][seatColumnIndex] = 1;
+                                System.out.println("Seat " + seatCodeToBook + " successfully booked.");
+                            } else {
+                                System.out.println("Seat " + seatCodeToBook + " is already booked.");
+                            }
+                        } else {
+                            System.out.println("Invalid seat code. Please try again.");
+                        }
+
+                        seats[seatRowIndex][seatColumnIndex] = 1;
+
+                    }
+                    System.out.println();
+                    break;
+                case 6:
+                    System.out.println("Thank you for using the Cinema Hall Seat Booking system. Goodbye!");
+                    return;
+
+            }
         }
 
     }
