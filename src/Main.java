d013c9a4702965927d0273ba29fd9400ffdd3763
Oudom Phoem <oudomphoem@gmail.com>
2024-12-10 23:03:00 +0700

    Create displaySeats method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
+
+    static void displaySeats(int rows, int columns, int[][] seats, char charRow) {
+        for (int i = 0; i < rows; i++) {
+            System.out.print("[");
+            for (int j = 0; j < columns; j++) {
+                String seatCode = charRow + "-" + (j + 1) + ":";
+                String bookStatus = "";
+                if (seats[i][j] == 0) {
+                    bookStatus = j == columns - 1 ? "AV" : "AV, ";
+                } else {
+                    bookStatus = j == columns - 1 ? "BO" : "BO, ";
+                }
+                System.out.print(seatCode + bookStatus);
+            }
+            System.out.print("]");
+            charRow++;
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -32,22 +52,8 @@ public class Main {
 
                     seats = new int[rows][columns];
 
-                    for (int i = 0; i < rows; i++) {
-                        System.out.print("[");
-                        for (int j = 0; j < columns; j++) {
-                            String seatCode = charRow + "-" + (j + 1) + ":";
-                            String bookStatus = "";
-                            if (seats[i][j] == 0) {
-                                bookStatus = j == columns - 1 ? "AV" : "AV, ";
-                            } else {
-                                bookStatus = j == columns - 1 ? "BO" : "BO, ";
-                            }
-                            System.out.print(seatCode + bookStatus);
-                        }
-                        System.out.print("]");
-                        charRow++;
-                        System.out.println();
-                    }
+                    displaySeats(rows, columns, seats, charRow);
+
                     System.out.println();
                     break;
                 case 2:
@@ -67,6 +73,7 @@ public class Main {
                             if (seats[seatRowIndex][seatColumnIndex] == 0) {
                                 seats[seatRowIndex][seatColumnIndex] = 1;
                                 System.out.println("Seat " + seatCodeToBook + " successfully booked.");
+                                displaySeats(rows, columns, seats, charRow);
                             } else {
                                 System.out.println("Seat " + seatCodeToBook + " is already booked.");
                             }
@@ -93,6 +100,7 @@ public class Main {
                             if (seats[seatRowIndex][seatColumnIndex] == 1) {
                                 seats[seatRowIndex][seatColumnIndex] = 0;
                                 System.out.println("Seat " + seatCodeToCancel + " successfully cancelled.");
+                                displaySeats(rows, columns, seats, charRow);
                             } else {
                                 System.out.println("Seat " + seatCodeToCancel + " is not book.");
                             }
@@ -103,33 +111,19 @@ public class Main {
                     System.out.println();
                     break;
                 case 4:
+                    charRow = 'A';
                     if (seats == null) {
                         System.out.println("Seats have not been set up. Please set up the seats first.");
                     } else {
-                        for (int i = 0; i < rows; i++) {
-                            System.out.print("[");
-                            for (int j = 0; j < columns; j++) {
-                                String seatCode = charRow + "-" + (j + 1) + ":";
-                                String bookStatus = "";
-                                if (seats[i][j] == 0) {
-                                    bookStatus = j == columns - 1 ? "AV" : "AV, ";
-                                } else {
-                                    bookStatus = j == columns - 1 ? "BO" : "BO, ";
-                                }
-                                System.out.print(seatCode + bookStatus);
-                            }
-                            System.out.print("]");
-                            charRow++;
-                            System.out.println();
-                        }
+                        displaySeats(rows, columns, seats, charRow);
                     }
-
                     System.out.println();
                     break;
                 case 6:
                     System.out.println("Thank you for using the Cinema Hall Seat Booking system. Goodbye!");
                     return;
-
+                default:
+                    System.out.println("Invalid option. Please choose again.");
             }
         }
 
